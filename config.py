6069781e9bde4b56ad8457a94a7964c11e9391cd
dir <op_@outlook.com>
2020-01-16 11:27:49 -0500

    Update and rename settings.py to config.py

--- config.py
+++ config.py
@@ -2,11 +2,14 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+if os.environ.get("ENVIRO") != "prod":
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
 
 ADMINS = os.environ.get("ADMINS")
 ADMINS = ADMINS.strip('][').split(', ')
+
+VDOCIPHER_SECRET = os.environ.get("VDOCIPHER_SECRET")
 
 FLASK_ENVIRONMENT = os.environ.get("FLASK_ENVIRONMENT")
 FLASK_SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
