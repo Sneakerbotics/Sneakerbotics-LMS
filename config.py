b920dde1860f4ad6e5ff286d0fa9e9ee61ec7a3d
dir <op_@outlook.com>
2020-01-18 16:41:03 -0500

    Admin fix

--- config.py
+++ config.py
@@ -2,12 +2,13 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+# VdoCipher Secret: xSyKGczUqGIRzc6vdpqLDO2R2zcYtPfZIoJImwTbeTDvD3px9Slmd4geUTvFWRqw
+
 if os.environ.get("ENVIRO") != "prod":
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
 
-ADMINS = os.environ.get("ADMINS")
-ADMINS = ADMINS.strip('][').split(', ')
+ADMIN = os.environ.get("ADMIN")
 
 VDOCIPHER_SECRET = os.environ.get("VDOCIPHER_SECRET")
 
